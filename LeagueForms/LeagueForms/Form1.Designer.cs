a5a5296b87713597f307e1da1385f5ff1db5ccf3
Evan <ewhite755@gmail.com>
2019-12-10 03:03:52 -0500

    baby steps

--- LeagueForms/LeagueForms/Form1.Designer.cs
+++ LeagueForms/LeagueForms/Form1.Designer.cs
@@ -31,7 +31,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.apiKey = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.usernameText = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.startButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -50,6 +52,7 @@
             this.apiKey.Name = "apiKey";
             this.apiKey.Size = new System.Drawing.Size(387, 20);
             this.apiKey.TabIndex = 1;
+            this.apiKey.TextChanged += new System.EventHandler(this.apiKey_TextChanged);
             // 
             // label2
             // 
@@ -60,20 +63,42 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Enter Username:";
             // 
-            // textBox1
+            // usernameText
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(16, 86);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(387, 20);
-            this.textBox1.TabIndex = 3;
+            this.usernameText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.usernameText.Location = new System.Drawing.Point(16, 86);
+            this.usernameText.Name = "usernameText";
+            this.usernameText.Size = new System.Drawing.Size(387, 20);
+            this.usernameText.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 153);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(387, 251);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // startButton
+            // 
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(16, 112);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_ClickAsync);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(415, 421);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.usernameText);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.apiKey);
             this.Controls.Add(this.label1);
@@ -89,7 +114,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox apiKey;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox usernameText;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button startButton;
     }
 }
 
--- LeagueForms/LeagueForms/Form1.cs
+++ LeagueForms/LeagueForms/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MingweiSamuel.Camille;
+using MingweiSamuel.Camille.Enums;
+using MingweiSamuel;
 
 namespace LeagueForms
 {
@@ -15,6 +18,30 @@ namespace LeagueForms
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void apiKey_TextChanged(object sender, EventArgs e)
+        {
+            startButton.Enabled = true;
+        }
+
+        private async void startButton_ClickAsync(object sender, EventArgs e)
+        {
+            var riotApi = RiotApi.NewInstance(apiKey.Text);
+            var summonerNameQuery = usernameText.Text;
+            var summonerData = await riotApi.SummonerV4.GetBySummonerNameAsync(MingweiSamuel.Camille.Enums.Region.NA, summonerNameQuery);
+            if (null == summonerData)
+            {
+                listBox1.Items.Add("No Summoner Found");
+                return;
+            }
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
