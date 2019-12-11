5d48b1d250105c3fdaa0be9d51b450b0b01b07c7
Evan <ewhite755@gmail.com>
2019-12-10 20:32:23 -0500

    finished?

--- LeagueForms/LeagueForms/Form1.Designer.cs
+++ LeagueForms/LeagueForms/Form1.Designer.cs
@@ -37,13 +37,15 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(9, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(96, 13);
             this.label1.TabIndex = 0;
@@ -52,7 +54,7 @@
             // apiKey
             // 
             this.apiKey.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.apiKey.Location = new System.Drawing.Point(16, 30);
+            this.apiKey.Location = new System.Drawing.Point(12, 29);
             this.apiKey.Name = "apiKey";
             this.apiKey.Size = new System.Drawing.Size(521, 20);
             this.apiKey.TabIndex = 1;
@@ -62,28 +64,30 @@
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 69);
+            this.label2.Location = new System.Drawing.Point(9, 61);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.Size = new System.Drawing.Size(225, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Enter Username:";
+            this.label2.Text = "Enter Username: (reg454 is my account name)";
             // 
             // usernameText
             // 
             this.usernameText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.usernameText.Location = new System.Drawing.Point(16, 86);
+            this.usernameText.Enabled = false;
+            this.usernameText.Location = new System.Drawing.Point(12, 77);
             this.usernameText.Name = "usernameText";
             this.usernameText.Size = new System.Drawing.Size(521, 20);
             this.usernameText.TabIndex = 3;
-            this.usernameText.Text = "reg454";
+            this.usernameText.TextChanged += new System.EventHandler(this.usernameText_TextChanged);
             // 
             // startButton
             // 
             this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.startButton.Location = new System.Drawing.Point(16, 112);
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(12, 153);
             this.startButton.Name = "startButton";
             this.startButton.Size = new System.Drawing.Size(75, 23);
-            this.startButton.TabIndex = 5;
+            this.startButton.TabIndex = 6;
             this.startButton.Text = "Start";
             this.startButton.UseVisualStyleBackColor = true;
             this.startButton.Click += new System.EventHandler(this.startButton_ClickAsync);
@@ -91,48 +95,90 @@
             // textBox1
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.textBox1.Location = new System.Drawing.Point(16, 169);
+            this.textBox1.Location = new System.Drawing.Point(12, 217);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(387, 240);
-            this.textBox1.TabIndex = 6;
+            this.textBox1.Size = new System.Drawing.Size(387, 192);
+            this.textBox1.TabIndex = 7;
             // 
             // textBox2
             // 
             this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(409, 169);
-            this.textBox2.Multiline = true;
+            this.textBox2.Location = new System.Drawing.Point(409, 217);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(128, 240);
-            this.textBox2.TabIndex = 7;
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(128, 20);
+            this.textBox2.TabIndex = 10;
             // 
             // label3
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 150);
+            this.label3.Location = new System.Drawing.Point(12, 201);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.Size = new System.Drawing.Size(116, 13);
             this.label3.TabIndex = 8;
-            this.label3.Text = "Match History";
+            this.label3.Text = "Ranked Match History:";
             // 
             // label4
             // 
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(406, 153);
+            this.label4.Location = new System.Drawing.Point(412, 201);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(121, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "Average CS Per Minute:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 126);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(255, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(258, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Select number of past matches you would like to see:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(549, 421);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
@@ -160,6 +206,8 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- LeagueForms/LeagueForms/Form1.cs
+++ LeagueForms/LeagueForms/Form1.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MingweiSamuel.Camille;
 using MingweiSamuel.Camille.Enums;
-using MingweiSamuel;
 
 
 namespace LeagueForms
@@ -24,11 +19,12 @@ namespace LeagueForms
 
         private void apiKey_TextChanged(object sender, EventArgs e)
         {
-            startButton.Enabled = true;
+            usernameText.Enabled = true;
         }
 
         public async void startButton_ClickAsync(object sender, EventArgs e)
         {
+            textBox1.Text = String.Empty;
             var riotApi = RiotApi.NewInstance(apiKey.Text);
             var summonerNameQuery = usernameText.Text;
             var summonerData = await riotApi.SummonerV4.GetBySummonerNameAsync(MingweiSamuel.Camille.Enums.Region.NA, summonerNameQuery);
@@ -38,13 +34,14 @@ namespace LeagueForms
                 return;
             }
 
-            var matchlist = await riotApi.MatchV4.GetMatchlistAsync(MingweiSamuel.Camille.Enums.Region.NA, summonerData.AccountId, queue: new[] { 420 }, endIndex: 10);
+            var pastmatches = comboBox1;
 
-            var matchDataTasks = matchlist.Matches.Select(
-                   matchMetadata => riotApi.MatchV4.GetMatchAsync(MingweiSamuel.Camille.Enums.Region.NA, matchMetadata.GameId)
-               ).ToArray();
+            var matchlist = await riotApi.MatchV4.GetMatchlistAsync(MingweiSamuel.Camille.Enums.Region.NA, summonerData.AccountId, queue: new[] { 420 }, endIndex: comboBox1.SelectedIndex+1);
+
+            var matchDataTasks = matchlist.Matches.Select(matchMetadata => riotApi.MatchV4.GetMatchAsync(MingweiSamuel.Camille.Enums.Region.NA, matchMetadata.GameId)).ToArray();
 
             var matchDatas = await Task.WhenAll(matchDataTasks);
+
             float sum = 0;
 
             for (var i = 0; i < matchDatas.Count(); i++)
@@ -55,6 +52,7 @@ namespace LeagueForms
 
                 var participant = matchData.Participants.First(p => p.ParticipantId == participantIdData.ParticipantId);
 
+
                 var win = participant.Stats.Win;
                 var champ = (Champion)participant.ChampionId;
                 var k = participant.Stats.Kills;
@@ -62,20 +60,33 @@ namespace LeagueForms
                 var a = participant.Stats.Assists;
                 var c = (participant.Stats.TotalMinionsKilled + participant.Stats.NeutralMinionsKilled);
                 var g = matchData.GameDuration;
+                var y = matchlist.TotalGames;
                 TimeSpan t = TimeSpan.FromSeconds(matchData.GameDuration);
                 string gametime = string.Format("{0:D2}:{1:D2}", t.Minutes, t.Seconds);
                 var kda = (k + a) / (float)d;
                 var cspm = (c / (float)g) * 60;
 
+  
 
                 textBox1.Text += String.Format("{0,3}) {1,-4} ({2}) Game Time: {3}\r\n K/D/A {4}/{5}/{6} ({7:0.00}) CS: {8} CSPM: {9:0.00}\r\n\r\n",
                     i + 1, win ? "Win" : "Loss", champ.Name(), gametime, k, d, a, kda, c, cspm);
 
                 sum += cspm;
                 textBox2.Text = (sum/matchlist.EndIndex).ToString("0.00");
+  
 
-                
+
             }
+        }
+
+        private void usernameText_TextChanged(object sender, EventArgs e)
+        {
+            comboBox1.Enabled = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            startButton.Enabled = true;
         }
     }
 }
